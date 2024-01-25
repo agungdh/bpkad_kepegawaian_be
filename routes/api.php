ae0430d98c58b0e115ff676883d98caef26f88d1
AgungDH <agungdh@live.com>
2024-01-25 11:14:51 +0700

    update composer

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Controller\PegawaiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/pegawai', PegawaiController::class);
 
 require __DIR__.'/auth.php';
