69bdcd43d0982c9315304939c1a95f3c91a95431
AgungDH <agungdh@live.com>
2024-01-25 16:07:17 +0700

    WIP: pegawai

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pegawai;
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pegawais = Pegawai::all();
+
+        return $pegawais;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $pegawai = new Pegawai();
+
+        $pegawai->nip = $request->nip;
+        $pegawai->nama = $request->nama;
+        $pegawai->tanggal_lahir = $request->tanggal_lahir;
+
+        $pegawai->save();
+
+        return $pegawai;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pegawai $pegawai)
+    {
+        //
+    }
+}
